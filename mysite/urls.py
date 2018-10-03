4f76f7e90a2459a10d52791bbb6984231135de30
rania <raniahelaoui23@gmail.com>
2018-10-03 13:24:58 +0100

    Modified templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-
+from blog.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
